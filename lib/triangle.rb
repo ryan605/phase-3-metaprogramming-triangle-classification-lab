9c57e3dc1492a8956ab3e592da7e62e9865669a8
ryan605 <langatryan26@gmail.com>
2023-02-18 23:46:35 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    arr = [a,b,c].sort
+    if (a <= 0 && b <= 0 && c <= 0) || (arr[0] + arr[1] <= arr[2]) || (a <= 0 || b <= 0 || c <= 0)
+      raise TriangleError
+    elsif a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+
+  class TriangleError < StandardError
+      "err"
+  end
 end
